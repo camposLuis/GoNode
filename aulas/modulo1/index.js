d5a65b78fce486f8ea40f69527384564cccb67eb
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-19 20:44:50 -0300

    Módulo 1: Fluxo de Requisições

--- aulas/modulo1/index.js
+++ aulas/modulo1/index.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  );
+
+  req.appName = "GoNode";
+
+  return next();
+};
+
+app.use(logMiddleware);
+
 app.get("/", (req, res) => {
-  return res.send(`Bem-Vindo, ${req.query.name}`);
+  return res.send(`Bem-Vindo ao ${req.appName}, ${req.query.name}`);
 });
 
 app.get("/nome/:name", (req, res) => {
