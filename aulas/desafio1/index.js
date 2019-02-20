6cedf4ca9443f04b3b1efb8d1f1bd66d06579dd9
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-20 20:46:57 -0300

    Finalização desafio 1

--- aulas/desafio1/index.js
+++ aulas/desafio1/index.js
@@ -3,8 +3,6 @@ const nunjucks = require('nunjucks')
 
 const app = express()
 
-const age = '0'
-
 nunjucks.configure('views', {
   autoescape: true,
   express: app,
@@ -14,17 +12,34 @@ nunjucks.configure('views', {
 app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'njk')
 
+const processadorIdade = (req, res, next) => {
+  const { idade } = req.query
+  if (idade === null || idade === '') {
+    return res.redirect('/')
+  }
+  return next()
+}
+
 app.get('/', (req, res) => {
-  return res.render('formulario', { age })
+  return res.render('formulario')
 })
 
-app.get('/maior', (req, res) => {
-  return res.render('maior', { age })
+app.get('/menor', processadorIdade, (req, res) => {
+  const { idade } = req.query
+  return res.render('menor', { idade })
 })
 
-app.post('/checagem', (req, res) => {
-  age.set(req.body.idade)
-  return res.redirect('maior', { age })
+app.get('/maior', processadorIdade, (req, res) => {
+  const { idade } = req.query
+  return res.render('maior', { idade })
+})
+
+app.post('/checagem', processadorIdade, (req, res) => {
+  const { idade } = req.body
+  if (idade < 18) {
+    return res.redirect(`/menor?idade=${idade}`)
+  }
+  return res.redirect(`/maior?idade=${idade}`)
 })
 
 app.listen(3000)
