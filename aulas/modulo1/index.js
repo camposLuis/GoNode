541a0d96099f9d5fce6a5a6a06f2cd587863c379
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-19 22:20:34 -0300

    Configurando ESlint

--- aulas/modulo1/index.js
+++ aulas/modulo1/index.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "njk");
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
 
-const users = ["Luís Campos", "Eliana de Fátima", "Bárbara Campos"];
+const users = ['Luís Campos', 'Eliana de Fátima', 'Bárbara Campos']
 
-app.get("/", (req, res) => {
-  return res.render("list", { users });
-});
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
 
-app.get("/new", (req, res) => {
-  return res.render("new");
-});
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
 
-app.post("/create", (req, res) => {
-  users.push(req.body.user);
-  return res.redirect("/");
-});
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
 
-app.listen(3000);
+app.listen(3000)
