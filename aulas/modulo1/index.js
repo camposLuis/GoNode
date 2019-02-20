96c086e84b4aa76cdef66649510b0964e4dcd82c
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-19 21:31:11 -0300

    Módulo 1: Enviando Formulário

--- aulas/modulo1/index.js
+++ aulas/modulo1/index.js
@@ -9,6 +9,7 @@ nunjucks.configure("views", {
   watch: true
 });
 
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "njk");
 
 const users = ["Luís Campos", "Eliana de Fátima", "Bárbara Campos"];
@@ -19,6 +20,11 @@ app.get("/", (req, res) => {
 
 app.get("/new", (req, res) => {
   return res.render("new");
+});
+
+app.post("/create", (req, res) => {
+  users.push(req.body.user);
+  return res.redirect("/");
 });
 
 app.listen(3000);
