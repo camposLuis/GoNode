9827690dd116884a7e257881487b545b8aa9bcde
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-22 09:11:04 -0300

    Upload de arquivos

--- aulas/modulo2/src/app/controllers/UserController.js
+++ aulas/modulo2/src/app/controllers/UserController.js
@@ -6,8 +6,9 @@ class UserController {
   }
 
   async store (req, res) {
-    req.body.avatar = 'teste.jpg'
-    await User.create(req.body)
+    const { filename: avatar } = req.file
+
+    await User.create(...req.body, avatar)
 
     return res.redirect('/')
   }
