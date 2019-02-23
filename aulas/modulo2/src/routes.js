5de6c48b48b90b4b9cdbe7061d5956a88477fc3a
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-23 15:03:28 -0300

    logout

--- aulas/modulo2/src/routes.js
+++ aulas/modulo2/src/routes.js
@@ -14,7 +14,10 @@ routes.post('/signin', SessionController.store)
 
 routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
 routes.use('/app', authMiddleware)
+
+routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
