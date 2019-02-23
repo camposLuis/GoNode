ca1e8e08a6b75d5e39abd25d410adc29576e85b0
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-22 23:33:04 -0300

    Autenticação

--- aulas/modulo2/src/app/models/Users.js
+++ aulas/modulo2/src/app/models/Users.js
@@ -21,5 +21,10 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
   )
+
+  User.prototype.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
+
   return User
 }
