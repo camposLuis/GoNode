23109e156d63371f18ce8a23436191ba3991b01e
camposLuis <luis.campos.henrique@hotmail.com>
2019-02-23 21:06:05 -0300

    Formulário de Agendamento

--- aulas/modulo2/src/routes.js
+++ aulas/modulo2/src/routes.js
@@ -10,6 +10,7 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentsController = require('./app/controllers/AppointmentsController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -21,15 +22,16 @@ routes.use((req, res, next) => {
 routes.get('/files/:file', FileController.show)
 
 routes.get('/', guestMiddleware, SessionController.create)
+
 routes.post('/signin', SessionController.store)
 
 routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
 routes.use('/app', authMiddleware)
-
 routes.get('/app/logout', SessionController.destroy)
-
 routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentsController.create)
 
 module.exports = routes
