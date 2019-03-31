b442cdbd4dfdae01d24bea3704a138304cb5ada6
camposLuis <luis.campos.henrique@hotmail.com>
2019-03-31 11:32:19 -0300

    Iniciando os testes

---
+++ modulo5/__tests__/integration/session.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+
+const app = require("../../src/app");
+const { User } = require("../../src/app/models");
+
+describe("Authentication", () => {
+  it("deve ser capaz de autenticar com credenciais válidas", async () => {
+    const user = User.create({
+      name: "Luís",
+      email: "luis@gmail.com",
+      password_hash: "luis"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "luis"
+      });
+
+    expect(response.status).toBe(200);
+  });
+});
--- modulo5/src/routes.js
+++ modulo5/src/routes.js
@@ -1,15 +1,7 @@
 const routes = require("express").Router();
 
-const { User } = require("./app/models");
+const SessionController = require("./app/controllers/SessionController");
 
-routes.get("/", async (req, res) => {
-  const user = await User.create({
-    name: "Luís Campos",
-    email: "luis@gmail.com",
-    password_hash: "luis"
-  });
-
-  return res.json({ user });
-});
+routes.post("/sessions", SessionController.store);
 
 module.exports = routes;
